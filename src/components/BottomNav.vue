a609b9f4ff8bda6776e52f5ab34a3b0fe24e0bfa
Dierfeus <zulin.zulin15@gmail.com>
2026-06-02 10:40:04 +0700

    нижний навбар с прокруткой

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -1,8 +1,61 @@
+<script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import {
+  House,
+  Calendar,
+  ChartColumn,
+  User
+} from 'lucide-vue-next'
+
+const route = useRoute()
+
+const translateX = computed(() => {
+  switch (route.path) {
+    case '/home':
+      return 0
+    case '/schedule':
+      return 108
+    case '/grades':
+      return 216
+    case '/profile':
+      return 324
+    default:
+      return 0
+  }
+})
+
+</script>
+
 <template>
-  <div class="bottom-nav">
-    <router-link to="/home">Главная</router-link>
-    <router-link to="/schedule">Расписание</router-link>
-    <router-link to="/grades">Оценки</router-link>
-    <router-link to="/profile">Профиль</router-link>
-  </div>
+  <nav class="bottom-nav">
+
+    <div
+        class="active-pill"
+        :style="{
+    transform: `translateX(${translateX}%)`
+  }"
+    ></div>
+
+    <router-link to="/home" class="nav-item">
+      <House :size="22" />
+    </router-link>
+
+    <router-link to="/schedule" class="nav-item">
+      <Calendar :size="22" />
+    </router-link>
+
+    <router-link to="/grades" class="nav-item">
+      <ChartColumn :size="22" />
+    </router-link>
+
+    <router-link to="/profile" class="nav-item">
+      <User :size="22" />
+    </router-link>
+
+  </nav>
 </template>
+
+
+
+
