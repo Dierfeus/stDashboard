e0822e6dd62997113368130aee779856b6d5c7db
Dierfeus <zulin.zulin15@gmail.com>
2026-06-02 10:39:06 +0700

    нижний навбар теперь на всем app а не отдельно на страницах

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import BottomNav from './components/BottomNav.vue'
+
+const route = useRoute()
+</script>
+
 <template>
   <router-view />
+
+  <BottomNav v-if="!route.meta.hideNav" />
 </template>
