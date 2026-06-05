36c7074a0952cb181c87e0fc48606b8471606165
What_is_${my}_name <pochtapodstav@gmail.com>
2026-06-05 15:01:36 +0700

    блин

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,61 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'logo.svg'],
+      manifest: {
+        name: 'Название вашего приложения',
+        short_name: 'AppName',
+        description: 'Описание приложения',
+        theme_color: '#42b883',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/stDashboard/',
+        start_url: '/stDashboard/',
+        icons: [
+          {
+            src: '/stDashboard/icons/icon-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: '/stDashboard/icons/icon-512x512.ico',
+            sizes: '512x512',
+            type: 'image/ico'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.example\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          }
+        ]
+      }
+    })
   ],
   base: '/stDashboard/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
